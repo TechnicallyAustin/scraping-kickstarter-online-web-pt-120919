a37929d7c661afbc5216452557bdbf14307dc351
Austin Johnson <technicallyaustin@gmail.com>
2020-02-03 06:36:41 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,24 @@
-# require libraries/modules here
+require "nokogiri"
+require "pry"
 
 def create_project_hash
-  # write your code here
+html = File.read('fixtures/kickstarter.html')
+kickstarter = Nokogiri::HTML(html)
+binding.pry
 end
+
+# projects selector:
+# kickstarter.css("li.project.grid_4")
+
+#title selector:
+# kickstarter.css("h2.bbcard_name strong a").text
+
+# image link:
+# kickstarter.css("div.project-thumbnail a img").attribute("src").value
+create_project_hash
+
+# description link 
+# kickstarter.css("p.bbcard_blurb").text 
+
+#location
+# kickstarter.css("span.location-name").first.text
