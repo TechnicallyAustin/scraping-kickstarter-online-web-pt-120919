ac1346b6d58c71b8505e2c89064d13a828fe48db
Austin Johnson <technicallyaustin@gmail.com>
2020-02-03 06:55:29 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -27,7 +27,7 @@ projects = {}
 kickstarter.css("li.project.grid_4").each do |project|
   title = project.css("h2.bbcard_name strong a").text 
   projects[title.to_sym] = { 
-    :image_link => project.css("li.project-thumbnail a img").attribute("src").value,
+    :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
     :description => project.css("p.bbcard_blurb").text,
     :location => project.css("span.location-name").text,
     :percent_funded => project.css("ul.project-stats.li.first.funded").text.gsub("%", "").to_i
